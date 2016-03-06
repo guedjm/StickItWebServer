08b82e75298f037c1e0b4fde8f636e8a93059838
=guedj_m <maxime.guedj@epitech.eu>
2016-03-06 00:10:59 -0800

    Remove typings stuff from git

--- typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser/ambient/bcrypt/bcrypt.d.ts" />
-/// <reference path="browser/ambient/express/express.d.ts" />
-/// <reference path="browser/ambient/mongoose/mongoose.d.ts" />
-/// <reference path="browser/ambient/node/node.d.ts" />
-/// <reference path="browser/ambient/vhost/vhost.d.ts" />
-/// <reference path="browser/ambient/winston/winston.d.ts" />
