88a2a01548beeccaff7a5df61a63808953e9c49b
=guedj_m <maxime.guedj@epitech.eu>
2016-03-06 00:10:02 -0800

    Install project dependencies and typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,6 @@
+/// <reference path="browser/ambient/bcrypt/bcrypt.d.ts" />
+/// <reference path="browser/ambient/express/express.d.ts" />
+/// <reference path="browser/ambient/mongoose/mongoose.d.ts" />
 /// <reference path="browser/ambient/node/node.d.ts" />
+/// <reference path="browser/ambient/vhost/vhost.d.ts" />
+/// <reference path="browser/ambient/winston/winston.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,1 +1,6 @@
+/// <reference path="main/ambient/bcrypt/bcrypt.d.ts" />
+/// <reference path="main/ambient/express/express.d.ts" />
+/// <reference path="main/ambient/mongoose/mongoose.d.ts" />
 /// <reference path="main/ambient/node/node.d.ts" />
+/// <reference path="main/ambient/vhost/vhost.d.ts" />
+/// <reference path="main/ambient/winston/winston.d.ts" />
