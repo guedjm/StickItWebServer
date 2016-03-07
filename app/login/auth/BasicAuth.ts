9da72c84f911771ace10f4ca0a895ec288db5952
=guedj_m <maxime.guedj@epitech.eu>
2016-03-07 05:05:30 -0800

    Add client-password passport rule

--- app/login/auth/BasicAuth.ts
+++ app/login/auth/BasicAuth.ts
@@ -6,6 +6,7 @@ import { IClientDocument, IClientDocumentModel } from "../model/Client";
 
 
 const BasicStrategy = require("passport-http").BasicStrategy;
+const ClientPasswordStrategy = require("passport-oauth2-client-password").ClientPasswordStrategy;
 
 passport.use('client-basic', new BasicStrategy((key: string, secret: string, done: Function): void => {
 
@@ -23,4 +24,20 @@ passport.use('client-basic', new BasicStrategy((key: string, secret: string, don
   });
 }));
 
-export const isBasicAuth = passport.authenticate('client-basic', { session: false });
+passport.use('client-password', new ClientPasswordStrategy((clientId: string, clientSecret: string, done: Function): void => {
+
+  const model: IClientDocumentModel = ModelManager.getClientModel();
+  model.findByClientIdAndSecret(clientId, clientSecret, (err: any, client: IClientDocument):void => {
+    if (err) {
+      done(err);
+    }
+    else if (client == undefined) {
+      done(null, false);
+    }
+    else {
+      done(null, client);
+    }
+  });
+}));
+
+export const isBasicAuth = passport.authenticate(['client-basic', 'client-password'], { session: false });
