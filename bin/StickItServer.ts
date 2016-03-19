6a46aaa422f2726d54d50ebd5f43dfc9957c6533
=guedj_m <maxime.guedj@epitech.eu>
2016-03-19 03:14:00 -0700

    Add test structure

--- bin/StickItServer.ts
+++ bin/StickItServer.ts
@@ -3,10 +3,10 @@
 import * as http from "http";
 import * as vHost from "vhost";
 import * as config from "config";
+import * as winston from "winston";
 import * as express from "express";
 import * as mongoose from "mongoose";
 import * as passport from "passport";
-import { info, error } from "winston";
 import StickItError from "../misc/Error";
 import * as bodyParser from "body-parser";
 import * as session from "express-session";
@@ -20,7 +20,7 @@ export default class StickItServer {
   private authApp: StickItAuthServer;
 
   public initialize(): void {
-    info("Initializing server");
+    winston.info("Initializing server");
     this._initializeExpress();
     this._initializeDatabaseConnection();
     this.server = http.createServer(this.exp);
@@ -29,26 +29,30 @@ export default class StickItServer {
   public start(): void {
 
     this.server.listen(config.get("server.port"));
-    info(`Server listening on port ${config.get("server.port")}`);
+    winston.info(`Server listening on port ${config.get("server.port")}`);
+  }
+
+  public disableLog(): void {
+    winston.level = "error";
   }
 
 
   private _initializeDatabaseConnection(): void {
-    info("Initializing database connection");
+    winston.info("Initializing database connection");
 
     mongoose.connection.on("open", function(): void {
-      info("Database connection initialized");
+      winston.info("Database connection initialized");
     });
 
     mongoose.connection.on("error", function(): void {
-      error("Unable to connect to the database");
+      winston.error("Unable to connect to the database");
     });
 
     mongoose.connect(`mongodb://${config.get("dbConfig.host")}:${config.get("dbConfig.port")}/${config.get("dbConfig.dbName")}`);
   }
 
   private _initializeExpress(): void {
-    info("Initializing express");
+    winston.info("Initializing express");
 
     this.exp = express();
 
@@ -71,7 +75,7 @@ export default class StickItServer {
     this.authApp.initialize();
     this.exp.use(vHost(config.get<string>("authServer.url"), this.authApp.routes));
 
-    this.exp.use(function(err: any, req: express.Request, res: express.Response): void {
+    this.exp.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction): void {
       if (!(err instanceof StickItError))
         console.error(err.stack);
       err = StickItError.internalServerError();
