e3f2e1b99d0fdfa2ec57545413234715b739a9a0
=guedj_m <maxime.guedj@epitech.eu>
2016-03-18 22:04:03 -0700

    Update login route

--- app/login/StickItAuthServer.ts
+++ app/login/StickItAuthServer.ts
@@ -19,19 +19,19 @@ export default class StickItAuthServer {
     info("Initializing StickItAuthServer");
     this.router = express.Router();
 
-    this.router.post("/client", ClientController.createClient);
-    this.router.get("/client", isBasicAuth, ClientController.hello);
+    this.router.post("/v1/oauth2/client", ClientController.createClient);
+    this.router.get("/v1/oauth2/client", isBasicAuth, ClientController.hello);
 
-    this.router.post("/user", isBasicAuth, UserController.createUser);
+    this.router.post("/v1/oauth2/user", isBasicAuth, UserController.createUser);
 
-    this.router.get("/login", LoginController.loginForm);
-    this.router.post("/login", validateLogin, LoginController.onUserLogin);
+    this.router.get("/v1/oauth2/login", LoginController.loginForm);
+    this.router.post("/v1/oauth2/login", validateLogin, LoginController.onUserLogin);
 
-    this.router.get("/authorize", OAuth2Controller.userLogged, ...OAuth2Controller.authorizationEndPoint,
+    this.router.get("/v1/oauth2/authorize", OAuth2Controller.userLogged, ...OAuth2Controller.authorizationEndPoint,
       LoginController.authorizeForm, OAuth2Controller.sserver.errorHandler);
-    this.router.post("/authorize", OAuth2Controller.userLogged, ...OAuth2Controller.decisionEndPoint);
+    this.router.post("/v1/oauth2/authorize", OAuth2Controller.userLogged, ...OAuth2Controller.decisionEndPoint);
 
-    this.router.post("/token", isBasicAuth, ...OAuth2Controller.tokenEndPoint);
+    this.router.post("/v1/oauth2/token", isBasicAuth, ...OAuth2Controller.tokenEndPoint);
   }
 
   get routes(): express.Router {
