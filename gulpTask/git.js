ff890fd14208aeef026228ea5322941d9590f7e1
=guedj_m <maxime.guedj@epitech.eu>
2016-03-22 12:05:09 -0700

    Test gulp task

--- gulpTask/git.js
+++ gulpTask/git.js
@@ -1,2 +1,23 @@
 "use strict";
 
+const gulp = require('gulp');
+const git = require('gulp-git');
+const prompt = require("gulp-prompt");
+
+gulp.task("commit", function () {
+  gulp.src("./")
+    .pipe(prompt.prompt({
+      type: 'input',
+      name: 'msg',
+      message: "Commit message :"
+    }, function (res) {
+      if (!res.msg || res.msg.length == 0) {
+        console.log("Please enter a valid commit message");
+      }
+      else {
+
+        gulp.src("./")
+          .pipe(git.commit(res.msg, {args: "-a"}))
+      }
+    }));
+});
