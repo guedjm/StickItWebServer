d9bfc1477d468ce922ef5be054bc4c79dd9e2ec8
=guedj_m <maxime.guedj@epitech.eu>
2016-03-22 12:25:43 -0700

    Add global bump command

--- gulpTask/bump.js
+++ gulpTask/bump.js
@@ -2,6 +2,31 @@
 
 const gulp = require("gulp");
 const bump = require("gulp-bump");
+const prompt = require("gulp-prompt");
+
+
+gulp.task("bump", function (cb) {
+  gulp.src("./")
+    .pipe(prompt.prompt({
+      type: "checkbox",
+      name: "bumpType",
+      message: "What type of bump would you like to do",
+      choices: ["patch", "minor", "major"],
+    }, function (res) {
+
+      if (!res.bumpType || res.bumpType.length != 1) {
+        console.log("Invalid bump type");
+      }
+      else {
+        const bumpType = res.bumpType[0];
+
+        gulp.src("./package.json")
+          .pipe(bump({ type: bumpType }))
+          .pipe(gulp.dest("./"));
+        cb();
+      }
+    }));
+});
 
 gulp.task("bump-patch", function () {
 
