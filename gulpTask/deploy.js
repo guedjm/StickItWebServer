9782a43e4b9a210dd81e57d7d7c40309e041252f
=guedj_m <maxime.guedj@epitech.eu>
2016-03-22 17:17:16 -0700

    Finalize gulp task

--- gulpTask/deploy.js
+++ gulpTask/deploy.js
@@ -1,7 +1,13 @@
 "use strict";
 
+const fs = require("fs");
 const gulp = require("gulp");
 const runSequence = require("run-sequence");
+
+gulp.task("push-success", function (cb) {
+  const pkg = JSON.parse(fs.readFileSync("./package.json"));
+  console.log("Version " + pkg.version + " successfuly pushed !");
+});
 
 gulp.task("push", function (cb) {
   runSequence(
@@ -10,14 +16,7 @@ gulp.task("push", function (cb) {
     "test",
     "bump",
     "exec-commit",
-    "exec-push", function (err) {
-      if (err) {
-        cb(err);
-      }
-      else {
-        const pkg = JSON.parse(fs.readFileSync("./package.json"));
-        console.log("Version " + pkg.version + " successfuly pushed");
-        cb(err);
-      }
-    });
+    "exec-push",
+    "push-success",
+    cb);
 });
