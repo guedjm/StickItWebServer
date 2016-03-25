6359f1daab0ff89fed351012cd4f1ab0f13399cd
=guedj_m <maxime.guedj@epitech.eu>
2016-03-24 19:50:40 -0700

    Add message to clean task

--- gulpTask/build.js
+++ gulpTask/build.js
@@ -7,12 +7,14 @@ const ts = require('gulp-typescript');
 gulp.task('build', function () {
   const project = ts.createProject('./tsconfig.json');
 
+  console.log("Building project ...");
   return project.src()
     .pipe(ts(project)).js
     .pipe(gulp.dest('build'));
 });
 
 gulp.task("clean", function () {
+  console.log("Cleaning project ...")
   return gulp.src("./build", { read: false})
     .pipe(clean());
 });
