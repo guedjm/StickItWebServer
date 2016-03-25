786f6e7e7b7c382b9010adfbee51c16653dd2e6c
=guedj_m <maxime.guedj@epitech.eu>
2016-03-25 14:00:50 -0700

    Add gulp task for creating user

--- gulpTask/db.js
+++ gulpTask/db.js
@@ -3,6 +3,7 @@
 const gulp = require("gulp");
 const config = require("config");
 const mongoose = require("mongoose");
+const prompt = require("gulp-prompt");
 
 gulp.task("db-test-clean", function (cb) {
 
@@ -74,6 +75,99 @@ gulp.task("db-test-init", function (cb) {
   });
 
   mongoose.connect(`mongodb://${config.get("dbConfig.host")}:${config.get("dbConfig.port")}/${config.get("test.dbConfig.dbName")}`);
+});
+
+gulp.task("db-create-user", function (cb) {
+
+  mongoose.connection.once("open", function () {
+    console.log("Database connection initialized");
+
+    gulp.src("./")
+      .pipe(prompt.prompt({
+        name: "email",
+        message: "Email :",
+        type: "input"
+      }, function (res) {
+
+        const email = res.email;
+        gulp.src("./")
+          .pipe(prompt.prompt({
+            name: "password",
+            message: "Password :",
+            type: "input"
+          }, function (res) {
+
+            const password = res.password;
+
+            const userDocumentModel = require("../build/src/app/login/model/User").userDocumentModel;
+            userDocumentModel.createUser(email, password, function (err, user) {
+              if (err) {
+                cb(err);
+              }
+              else {
+                console.log("User created");
+                console.log(user);
+                mongoose.disconnect( function () {
+                  cb();
+                });
+              }
+            });
+          }));
+      }));
+  });
+
+  mongoose.connection.once("error", function () {
+    console.error("Unable to connect to the database");
+  });
+
+  mongoose.connect(`mongodb://${config.get("dbConfig.host")}:${config.get("dbConfig.port")}/${config.get("dbConfig.dbName")}`);
+
+});
+
+gulp.task("db-create-client", function (cb) {
+  mongoose.connection.once("open", function () {
+    console.log("Database connection initialized");
+
+    gulp.src("./")
+      .pipe(prompt.prompt({
+        name: "name",
+        message: "Name :",
+        type: "input"
+      }, function (res) {
+
+        const name = res.name;
+        gulp.src("./")
+          .pipe(prompt.prompt({
+            name: "ctype",
+            message: "Type :",
+            type: "checkbox",
+            choices: ["1", "2", "3"]
+          }, function (res) {
+
+            const type = res.ctype;
+
+            const clientDocumentModel = require("../build/src/app/login/model/Client").clientDocumentModel;
+            clientDocumentModel.createClient(name, parseInt(type), function (err, client) {
+              if (err) {
+                cb(err);
+              }
+              else {
+                console.log("Client created");
+                console.log(client);
+                mongoose.disconnect( function () {
+                  cb();
+                });
+              }
+            });
+          }));
+      }));
+  });
+
+  mongoose.connection.once("error", function () {
+    console.error("Unable to connect to the database");
+  });
+
+  mongoose.connect(`mongodb://${config.get("dbConfig.host")}:${config.get("dbConfig.port")}/${config.get("dbConfig.dbName")}`);
 });
 
 gulp.task("wait", function (cb) {
